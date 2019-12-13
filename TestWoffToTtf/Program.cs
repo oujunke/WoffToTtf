a8c818007e62f57970a3ccf5df246a37f8f7c156
oujk <oujk@ejiaofei.com>
2019-12-13 17:41:58 +0800

    添加项目文件。

---
+++ TestWoffToTtf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestWoffToTtf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WoffToTtf/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WoffToTtf
+{
+    public class Class1
+    {
+    }
+}
